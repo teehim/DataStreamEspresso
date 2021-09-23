dacb3b76248af1145cb59f510311240600480691
teehim <teehim.thanatkorn@gmail.com>
2021-09-23 23:22:19 +0700

    clear old file

--- quiz1/question1/python_prod_cons/read_harry.py
+++
@@ -1,9 +0,0 @@
-import time
-from kafka import KafkaProducer
-
-producer = KafkaProducer(bootstrap_servers=['localhost:9092','localhost:9192','localhost:9292'])
-
-with open('./text/book.txt', 'r', newline='', encoding='utf-8') as file:
-    for line in file.readlines():
-        producer.send('read-harry-stream', line.strip().encode('utf-8'))
-        time.sleep(1)
